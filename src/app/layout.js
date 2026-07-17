b8abbd087df786879f2ffc226d73bbf31b60eaa4
Brian Hicks <hi@bazyinc.com>
2026-07-17 15:26:50 -0400

    Added Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Manrope } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "../app/globals.css";
 import Header from "../components/Header"; 
 import Footer from "../components/Footer";
